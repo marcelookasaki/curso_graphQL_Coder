d9ebf053a66f49e600547e6f884491b42c404375
marcelookasaki <marcelo.okasaki@gmail.com>
2022-05-26 16:14:04 -0300

    Resolver de usuário

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+
   scalar Date
+
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   # Pontos de entrada da API
   type Query {
     ola: String
     horaAtual: Date
+    usuarioLogado: Usuario
   }
 `
 
 const resolvers = {
+  Usuario: {
+    salario(usuario) {
+      return usuario.salario_real
+    }
+  },
   Query: {
     ola() {
       return 'Bom dia!'
     },
     horaAtual() {
       return new Date
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'Marcelo',
+        email: 'marcelo.okasaki@email.com',
+        idade: 54,
+        salario_real: 100000.19,
+        vip: true
+      }
+
     }
 
   }
